d7ca279f60d2a840db455237bff9fbee0b8184d6
Abishek Raghavan <abishek.raghavan@kellogg.northwestern.edu>
2023-05-15 21:28:17 +0000

    v2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,8 @@ class PostsController < ApplicationController
   def create
     @post = Post.new 
     @post["name"] = params["post"]["name"]
+    @post["description"] = params["post"]["description"]
+    @post["posted_on"] = params["post"]["date"]
     @post.save
     redirect_to "/posts"
 
