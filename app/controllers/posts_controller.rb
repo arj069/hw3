26905759324c20b44df2adc451d71ad245dfc09c
Abishek Raghavan <abishek.raghavan@kellogg.northwestern.edu>
2023-05-16 16:13:01 +0000

    v7

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new 
-    @place = Place.find_by({"name"=>params["id"]})
+    @place = Place.find_by({"id"=>params["place_id"]})
     @post["place_id"] = @place["id"]
   end
 
@@ -15,8 +15,9 @@ class PostsController < ApplicationController
     @post["title"] = params["post"]["title"]
     @post["description"] = params["post"]["description"]
     @post["posted_on"] = params["post"]["date"]
+    @post["place_id"] = params["post"]["place_id"]
     @post.save
-    redirect_to "/places"
+    redirect_to "/places/#{@post["place_id"]}"
   end
 
 end
